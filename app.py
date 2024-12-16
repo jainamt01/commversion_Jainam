985adbafc0173f4e02b983ffd35c618a9971a489
jainamt01 <82870739+jainamt01@users.noreply.github.com>
2024-12-16 17:57:20 +0530

    app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title("Metabase Dashboard Embedding")
 
 # Embed Metabase Dashboard
 metabase_url = "https://commversion.metabaseapp.com/collection/10-client-dashboard-2-facts-v2"
-st.markdown(f'<iframe src="{metabase_url}" width="800" height="600" frameborder="0"></iframe>', unsafe_allow_html=True)
+st.markdown(f'<iframe src="{metabase_url}" width="1200" height="600" frameborder="0"></iframe>', unsafe_allow_html=True)
