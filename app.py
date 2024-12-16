eb5071cfc798e8cd5ef0f24220375b0f12f28497
jainamt01 <82870739+jainamt01@users.noreply.github.com>
2024-12-16 17:28:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Title of the app
+st.title("Metabase Dashboard Embedding")
+
+# Embed Metabase Dashboard
+metabase_url = "/collection/10-client-dashboard-2-facts-v2"
+st.markdown(f'<iframe src="{metabase_url}" width="800" height="600" frameborder="0"></iframe>', unsafe_allow_html=True)
