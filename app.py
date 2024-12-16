5552e74b1a533e6b9cc7c91e8b4ea8bb09ef98e2
jainamt01 <82870739+jainamt01@users.noreply.github.com>
2024-12-16 17:41:18 +0530

    app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title("Metabase Dashboard Embedding")
 
 # Embed Metabase Dashboard
-metabase_url = "https://commversion.metabaseapp.com/dashboard/13-client-dashboard?text=0018d00000Mh0CHAAZ&time_based_filtering="
+metabase_url = "https://commversion.metabaseapp.com/public/dashboard/b0ae71a8-1beb-4d14-87df-eab23e66b76f"
 st.markdown(f'<iframe src="{metabase_url}" width="800" height="600" frameborder="0"></iframe>', unsafe_allow_html=True)
